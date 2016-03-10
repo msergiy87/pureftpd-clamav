b05957aa488fd8cb5c6819bd13a064e5f0b98a70
Sergiy <sergiy_007@ukr.net>
2016-03-10 13:24:29 +0200

    Update clamav_check.sh

--- clamav_check.sh
+++ clamav_check.sh
@@ -26,16 +26,16 @@ fi
 # Exclude FTP requests from temporary files
 #------------------------------------------------------------------------------------------
 echo "$1" | grep -E "\.tmp$" > /dev/null 2>&1				# exclude
-if [ $? -ne 0 ]								# if not equal, not success
+if [ 0 -ne $? ]								# if not equal, not success
 then
 
 # Scan the uploaded file. Move to quarantine if suspicious
 #------------------------------------------------------------------------------------------
 
-	/usr/bin/clamdscan -i --move=/tmp/quarantine --fdpass --no-summary --stdout>$OUT "$1"
+	clamdscan -i --move=/tmp/quarantine --fdpass --quiet --no-summary --stdout>$OUT "$1"
 
-	#/usr/bin/clamdscan --move=/tmp/quarantine --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
-	#/usr/bin/clamdscan --remove --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
+	# clamdscan --move=/tmp/quarantine --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
+	# clamdscan --remove --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
 
 else
 	exit 0
@@ -45,34 +45,35 @@ CODE="$?"
 
 # send email with viruses
 #------------------------------------------------------------------------------------------
-if [ "$CODE" -eq 1 ]							# equal 
+if [ 1 -eq "$CODE" ]							# equal 
 then
 	echo "Date: $DATE" > $EMAILMESSAGE
+
 	{
 		echo "Foud some viruses during transfer FTP in"
 		echo "----------------------------------------------------------"
 		cat $OUT
-		echo ""
-	}  >> $EMAILMESSAGE
+	} >> $EMAILMESSAGE
+
 	cat $EMAILMESSAGE >> $ALLOG
-#	echo "Please, run for check /usr/bin/clamscan -i -r /tmp/quarantine" >> $EMAILMESSAGE
-	/usr/bin/mail -s "Vriruses $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
-	/bin/chmod 000 /tmp/quarantine/* > /dev/null 2>&1
+	echo "" >> $ALLOG
+#	echo "Please, run for check clamscan -i -r /tmp/quarantine" >> $EMAILMESSAGE
+	mail -s "Vriruses $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
+	chmod 000 /tmp/quarantine/* > /dev/null 2>&1
 
 # send email with errors
 #------------------------------------------------------------------------------------------
-elif [ "$CODE" -eq 2 ]							# equal 
+elif [ 2 -eq "$CODE" ]							# equal 
 then
 	echo "Date: $DATE" > $EMAILMESSAGE
+
 	{
 		echo "Foud some errors during scanning from ClamAV transfer FTP in"
 		echo "----------------------------------------------------------"
 		cat $OUT
-		echo ""
 	} >> $EMAILMESSAGE
-	cat $EMAILMESSAGE >> $ALLOG
-#	/usr/bin/mail -s "Errors $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
 
-else
-	exit 0
+	cat $EMAILMESSAGE >> $ALLOG
+	echo "" >> $ALLOG
+#	mail -s "Errors $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
 fi
