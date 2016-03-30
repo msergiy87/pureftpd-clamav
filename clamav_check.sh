0069325c0aa2f0be3fc7180da48668fce4840d39
Sergiy <sergiy_007@ukr.net>
2016-03-30 18:44:43 +0300

    Update clamav_check.sh

--- clamav_check.sh
+++ clamav_check.sh
@@ -13,6 +13,7 @@ SUBJECT="detected by ClamAV from FTP on"
 EMAILMESSAGE="/tmp/quarantine/ftp_mail.log"
 OUT="/tmp/quarantine/ftp_out.log"
 ALLOG="/var/log/clamav/ftp-clamdscan.log"
+SCAN_FILE="$1"
 
 # check conditions
 #------------------------------------------------------------------------------------------
@@ -41,20 +42,21 @@ fi
 
 # Exclude FTP requests from temporary files
 #------------------------------------------------------------------------------------------
-echo "$1" | grep -E "\.tmp$" > /dev/null 2>&1				# exclude
+echo "$SCAN_FILE" | grep -E "\.tmp$" > /dev/null 2>&1			# exclude
 if [ 0 -ne $? ]								# if not equal, not success
 then
 
-# Scan the uploaded file. Move to quarantine if suspicious
-#------------------------------------------------------------------------------------------
-
-	clamdscan -i --move=/tmp/quarantine --fdpass --no-summary --stdout>$OUT "$1"
+	# Scan the uploaded file. Move to quarantine if suspicious
+	clamdscan -i --move=/tmp/quarantine --fdpass --no-summary --stdout>$OUT "$SCAN_FILE"
 
 	# clamdscan --move=/tmp/quarantine --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
 	# clamdscan --remove --fdpass --quiet --no-summary --log=/var/log/removed_files.log "$1"
 
 else
-	exit 0
+	echo "Date: $DATE" >> $ALLOG
+	echo "Exclude tmp file $SCAN_FILE from scanning" >> $ALLOG
+	echo "" >> $ALLOG
+	exit 113
 fi
 
 CODE="$?"
@@ -66,14 +68,13 @@ then
 	echo "Date: $DATE" > $EMAILMESSAGE
 
 	{
-		echo "Foud some viruses during transfer FTP in"
+		echo "Foud some viruses during scanning $SCAN_FILE in FTP transfer"
 		echo "----------------------------------------------------------"
 		cat $OUT
 	} >> $EMAILMESSAGE
 
 	cat $EMAILMESSAGE >> $ALLOG
 	echo "" >> $ALLOG
-#	echo "Please, run for check clamscan -i -r /tmp/quarantine" >> $EMAILMESSAGE
 	mail -s "Vriruses $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
 	chmod 000 /tmp/quarantine/* > /dev/null 2>&1
 
@@ -84,7 +85,7 @@ then
 	echo "Date: $DATE" > $EMAILMESSAGE
 
 	{
-		echo "Foud some errors during scanning from ClamAV transfer FTP in"
+		echo "Foud some ERRORS during scanning $SCAN_FILE in FTP transfer"
 		echo "----------------------------------------------------------"
 		cat $OUT
 	} >> $EMAILMESSAGE
@@ -92,4 +93,16 @@ then
 	cat $EMAILMESSAGE >> $ALLOG
 	echo "" >> $ALLOG
 #	mail -s "Errors $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
+
+else
+	echo "Date: $DATE" > $EMAILMESSAGE
+	{
+		echo "Foud some PROBLEMS during scanning $SCAN_FILE in FTP transfer. End code is $CODE"
+		echo "----------------------------------------------------------"
+		cat $OUT
+	} >> $EMAILMESSAGE
+
+	cat $EMAILMESSAGE >> $ALLOG
+	echo "" >> $ALLOG
+#	mail -s "Problems $SUBJECT $HOST" "$EMAILTO" < $EMAILMESSAGE
 fi
